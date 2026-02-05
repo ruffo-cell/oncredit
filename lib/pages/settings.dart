1c55628ec2bbe20e7238aeb0c110f0c4212a90b5
André Luferat <contato@luferat.net>
2026-02-05 11:44:35 -0300

    Update settings.dart

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,13 +1,10 @@
 // lib/pages/settings.dart
 
 import 'package:flutter/material.dart';
-import '../models/client.dart';
 import '../services/client_service.dart';
 import '../templates/appbar.dart';
 import '../config/app_config.dart';
-import '../tools/formatters.dart';
 import '../services/finance_service.dart';
-import 'client_page.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -17,25 +14,11 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-  late final String _status;
-
-  final FinanceService _financeService = FinanceService();
-  final ClientService _clientService = ClientService();
-
-  final TextEditingController _searchController = TextEditingController();
-  String _search = '';
 
   @override
   void initState() {
     super.initState();
-    _status = 'UID ativo: ${AppConfig.fixedUid}';
-  }
-
-  @override
-  void dispose() {
-    _searchController.dispose();
-    super.dispose();
-  }
+   }
 
   @override
   Widget build(BuildContext context) {
