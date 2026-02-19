678dd36b34fa7b6329355c2dc688bfbbbf7992c5
ruffo-cell <ruffofla@gmail.com>
2026-02-19 09:25:09 -0300

    finanças

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,24 +1,11 @@
 // lib/config/app_config.dart
 
-import 'package:shared_preferences/shared_preferences.dart';
+
 
 class AppConfig {
   static const String environment = 'DEV'; // ou PROD
   static String fixedUid = 'dev_uid_001';
-  static String baseUrl = 'https://jsbpad-default-rtdb.firebaseio.com';
+  static String baseUrl = 'https://springpad-19338-default-rtdb.firebaseio.com';
 
-  static Future<void> load() async {
-    final prefs = await SharedPreferences.getInstance();
-    fixedUid = prefs.getString('fixedUid') ?? fixedUid;
-    baseUrl = prefs.getString('baseUrl') ?? baseUrl;
-  }
 
-  static Future<void> save(String uid, String url) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString('fixedUid', uid);
-    await prefs.setString('baseUrl', url);
-
-    fixedUid = uid;
-    baseUrl = url;
-  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,10 @@
-// lib/main.dart
+// lib\main.dart
 
 import 'package:flutter/material.dart';
 import 'package:oncredit/pages/home.dart';
 import 'package:oncredit/pages/settings.dart';
-import 'package:oncredit/theme/theme_controller.dart';
-import 'config/app_config.dart';
 
-late ThemeController themeController;
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await AppConfig.load();
-
-  themeController = ThemeController(); // ← aqui usa a global
-  await themeController.loadTheme();
-
+void main() {
   runApp(const MyApp());
 }
 
@@ -23,27 +13,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: themeController,
-      builder: (_, __) => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        themeMode: themeController.themeMode,
-        theme: ThemeData(
-          brightness: Brightness.light,
-          primarySwatch: Colors.deepPurple,
-          useMaterial3: true,
-        ),
-        darkTheme: ThemeData(
-          brightness: Brightness.dark,
-          colorSchemeSeed: Colors.deepPurple,
-          useMaterial3: true,
-        ),
-        initialRoute: '/',
-        routes: {
-          '/': (context) => const HomePage(),
-          '/settings': (context) => const SettingsPage(),
-        },
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'OnCredit',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.deepPurple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+
+      initialRoute: '/',
+
+      routes: {
+        // Página inicial
+        '/': (context) => const HomePage(),
+        // Configurações
+        '/settings': (context) => const SettingsPage(),
+      },
     );
   }
 }
